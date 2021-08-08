7cb18a385145745e06ffd2244a6f8f6fba293e01
Geoff Holden <geoff@geoffholden.com>
2021-08-08 09:21:16 -0230

    Update android sdk paths

--- android/path.zsh
+++ android/path.zsh
@@ -2,12 +2,13 @@ if [ -d ${HOME}/Android/Sdk ]; then
     export ANDROID_HOME=${HOME}/Android/Sdk
 elif [ -d /usr/local/opt/android-sdk ]; then
     export ANDROID_HOME=/usr/local/opt/android-sdk
-elif [ -d /Developer/android-sdk-macosx ]; then
-    export ANDROID_HOME=/Developer/android-sdk-macosx
+elif [ -d /Applications/Developer/android-sdk-macosx ]; then
+    export ANDROID_HOME=/Applications/Developer/android-sdk-macosx
 fi
 
 if [ ! -z $ANDROID_HOME ]; then
     path+=(${ANDROID_HOME}/tools)
+    path+=(${ANDROID_HOME}/tools/bin)
     path+=(${ANDROID_HOME}/platform-tools)
     path+=(${ANDROID_HOME}/tools/proguard/bin)
 fi
